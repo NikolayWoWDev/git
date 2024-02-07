8b379a636c36dd2161226ca680169ac8a5c630f2
Nikolay <zerogame221@gmail.com>
2024-02-07 07:33:15 +0200

    Randomize

--- js game/main.js
+++ js game/main.js
@@ -1,147 +1,161 @@
-const g_Character = 
-{
-	name: "Pikachu",
-	level: getSavedLevel(),
-	health: 100,
-	maxHealth: 100,
-	element: document.getElementById('character'),
-	progressBar: document.getElementById('progressbar-character'),
-	healthText: document.getElementById('health-character'),
-	Kicks: 6,
-	Attacks: 6,
-	
-	updateDetails: function() {
-		this.healthText.textContent = `${this.health} / ${this.maxHealth}`;
-		const healthPercentage = (this.health / this.maxHealth) * 100;
-		this.progressBar.style.width = `${healthPercentage}%`;
-	},
-	
-	getMaxHealth: function(p_Level) 
-	{
-        if (p_Level < 5) 
-            return 100;
-     
-        return Math.round(100 * ((p_Level * 1.5) / (p_Level + 2)));
-    },
-	
-	updateMaxHealth: function() {
-        this.maxHealth = this.getMaxHealth(this.level);
-		this.healthText.textContent = `${this.health} / ${this.maxHealth}`;
-    },
-	
-	attack: function(p_Type) 
-	{
-	const l_Damage = Math.floor(Math.random() * 20) + 10;
-	l_EnemyDamage = Math.floor(Math.random() * 15) + 5;
-	switch(p_Type) 
-	{
-	  case 'kick':
-		this.Kicks--;
-		
-		if(!CanUse(this.Kicks))
-			return;
-		
-		g_Enemy.health -= l_Damage;
-		break;
-	  case 'other':
-		this.Attacks--;
-		
-		if(!CanUse(this.Attacks))
-			return;
-		
-		g_Enemy.health -= l_Damage * 2;
-		break;
-	  case 'die':
-		l_EnemyDamage = 100;
-		break;
-	}
-	
-	animateShake(g_Enemy.healthText);
-
-	if (g_Enemy.health <= 0) {
-	  g_Enemy.health = 0;
-	  setGameState('Win');
-	}
-
-	g_Enemy.updateDetails();
-	
-	
-	animateShake(this.healthText);
-	this.health -= l_EnemyDamage;
-
-	if (this.health <= 0) {
-	  this.health = 0;
-	  setGameState('Lose');
-	}
-	
-	this.updateDetails();
-	logBattle(l_EnemyDamage, g_Enemy.health);
-	}
-};
-
-const g_Enemy = 
-{
-	name: "Charmander",
-	level: Math.floor(Math.random() * ((getSavedLevel() + 1) - (getSavedLevel() - 1) + 1)) + (getSavedLevel() - 1),
-	health: 100,
-	maxHealth: 100,
-	element: document.getElementById('enemy'),
-	progressBar: document.getElementById('progressbar-enemy'),
-	healthText: document.getElementById('health-enemy'),
-	updateDetails: g_Character.updateDetails,
-	updateMaxHealth: function() {
-        this.maxHealth = g_Character.getMaxHealth(this.level);
-		this.healthText.textContent = `${this.health} / ${this.maxHealth}`;
+class Pokemon {
+	constructor(name, elementId, progressBarId, healthTextId, imgId, imgUrl, hp) {
+        this.name = name;
+        this.level = getSavedLevel();
+        this.health = this.getMaxHealth(this.level, hp || 100); // Используем данные из массива pokemons
+        this.maxHealth = this.health;
+        this.element = document.getElementById(elementId);
+        this.progressBar = document.getElementById(progressBarId);
+        this.healthText = document.getElementById(healthTextId);
+        this.Img = document.getElementById(imgId);
+        this.ImgUrl = imgUrl;
+        this.Img.src = this.ImgUrl;
+        this.kicks = 6;
+        this.attacks = 6;
+        const enemyNameElement = document.getElementById('name-enemy');
+        enemyNameElement.textContent = this.name;
     }
-};
+	
+    updateDetails() {
+        this.healthText.textContent = `${this.health} / ${this.maxHealth}`;
+        const healthPercentage = (this.health / this.maxHealth) * 100;
+        this.progressBar.style.width = `${healthPercentage}%`;
+		
+		if(healthPercentage <= 60 && healthPercentage >= 20)
+			this.progressBar.classList.add('low');
+		else if (healthPercentage < 20)
+			this.progressBar.classList.add('critical');
+		
+		this.Img.src = this.ImgUrl;
+    }
 
-function setGameState(p_State) 
-{
-	const l_BtnControl = document.querySelector('.control');
-	switch(p_State) 
-	{
-	case 'Win':
-		const newLevel = getSavedLevel() + 1;
-        saveLevel(newLevel);
-		l_BtnControl.innerHTML = '<button class="button" onclick="restart()">Вы победили</button>';
-		break;
-	case 'Lose':
-		saveLevel(1);
-		l_BtnControl.innerHTML = '<button class="button" onclick="restart()">Вы проиграли</button>';
-		break;
-	}
+    getMaxHealth(level, hp) {
+        if (level < 5) {
+            return hp;
+        }
+        return Math.round(hp * (1 + level / (level + 2)));
+    }
+
+    updateMaxHealth() {
+        this.maxHealth = this.getMaxHealth(this.level, this.health);
+        this.healthText.textContent = `${this.health} / ${this.maxHealth}`;
+    }
+
+    attack(type, enemy, cheat) {
+        let l_Damage = Math.floor(Math.random() * 20) + 50;
+		
+		const l_RandomAttackIndex = Math.floor(Math.random() * enemy.attacks.length);
+        const l_SelectedAttack = enemy.attacks[l_RandomAttackIndex];
+        let l_EnemyDamage = Math.floor(Math.random() * (l_SelectedAttack.maxDamage - l_SelectedAttack.minDamage + 1)) + l_SelectedAttack.minDamage;
+		if(cheat)
+		{
+			l_Damage = enemy.health;
+			l_EnemyDamage = 0;
+		}
+
+        switch (type) {
+            case 'kick':
+                this.kicks--;
+                if (!this.canUse(this.kicks)) return;
+                enemy.health -= l_Damage;
+                break;
+            case 'other':
+                this.attacks--;
+                if (!this.canUse(this.attacks)) return;
+                enemy.health -= l_Damage * 2;
+                break;
+        }
+
+        animateShake(enemy.healthText);
+
+        if (enemy.health <= 0) 
+            enemy.health = 0;
+
+        enemy.updateDetails();
+
+        animateShake(this.healthText);
+		this.health -= l_EnemyDamage;
+
+        if (this.health <= 0 && enemy.health > 0)	// На случай если противник победил
+		{
+            this.health = 0;
+            setGameState('Lose');
+        } else if (this.health <= 0 && enemy.health <= 0)
+		{
+			this.health = 0;
+            setGameState('None');
+		} else if (enemy.health == 0 && this.health > 0)
+			setGameState('Win');
+			
+
+        this.updateDetails();
+        logBattle(l_EnemyDamage, enemy.health);
+    }
+
+    canUse(attackType) {
+        gClicks++;
+        console.log("Всего кликов " + gClicks);
+
+        if (attackType < 0) {
+            console.log("Данная способность закончилась");
+            return false;
+        }
+
+        console.log("Осталось " + attackType + " использований");
+
+        return true;
+    }
 }
 
-function CanUse(p_AttackType)
-{
-	gClicks++;
-	
-	console.log("Всего кликов " + gClicks);
-	
-	if(p_AttackType < 0)
-	{
-		console.log("Данная способность закончилась");
-		return false;
-	}
-	
-	console.log("Осталось " + p_AttackType + " использований");
-	
-	return true;
+const pikachuData = pokemons[0];
+const g_Character = new Pokemon("Pikachu", 'character', 'progressbar-character', 'health-character', 'hero_img', 'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png', pikachuData.hp);
+const g_Enemy = getRandomPokemon(pokemons);
+
+function setGameState(p_State) {
+    const l_BtnControl = document.querySelector('.control');
+    switch (p_State) {
+        case 'Win':
+            const newLevel = getSavedLevel() + 1;
+            saveLevel(newLevel);
+            l_BtnControl.innerHTML = '<button class="button" onclick="restart()">Вы победили</button>';
+            break;
+        case 'Lose':
+            saveLevel(1);
+            l_BtnControl.innerHTML = '<button class="button" onclick="restart()">Вы проиграли</button>';
+            break;
+		case 'None':
+			l_BtnControl.innerHTML = '<button class="button" onclick="restart()">Ничья</button>';
+			break;
+    }
+}
+
+function CanUse(p_AttackType) {
+    gClicks++;
+
+    console.log("Всего кликов " + gClicks);
+
+    if (p_AttackType < 0) {
+        console.log("Данная способность закончилась");
+        return false;
+    }
+
+    console.log("Осталось " + p_AttackType + " использований");
+
+    return true;
 }
 
 function logBattle(p_Damage, p_RemainingHealth) {
-	const l_BattleLog = document.getElementById('logs');
+    const l_BattleLog = document.getElementById('logs');
     const l_LogEntry = document.createElement('div');
-    const l_LogIndex = Math.floor(Math.random() * logs.length); 
+    const l_LogIndex = Math.floor(Math.random() * logs.length);
     const l_LogMessage = logs[l_LogIndex];
     const l_FormattedLog = l_LogMessage.replace('[ПЕРСОНАЖ №1]', g_Character.name).replace('[ПЕРСОНАЖ №2]', g_Enemy.name)
     l_LogEntry.textContent = `${l_FormattedLog} Урон: ${p_Damage}. Осталось жизней соперника: ${p_RemainingHealth}`;
     l_BattleLog.insertBefore(l_LogEntry, l_BattleLog.firstChild);
 }
 
-function restart() 
-{
-location.reload();
+function restart() {
+    location.reload();
 }
 
 function saveLevel(level) {
@@ -149,7 +163,7 @@ function saveLevel(level) {
 }
 
 function getSavedLevel() {
-    return parseInt(localStorage.getItem('characterLevel')) || 1; // Если уровень не сохранен, возвращаем 1
+    return parseInt(localStorage.getItem('characterLevel')) || 1;
 }
 
 function animateShake(element) {
@@ -159,12 +173,6 @@ function animateShake(element) {
     }, 500);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	g_Character.updateMaxHealth(g_Character.level);
-	g_Enemy.updateMaxHealth(g_Enemy.level);
-	setCharacterAndEnemyLevels();
-});
-
 function setCharacterAndEnemyLevels() {
     const l_CharacterLevelElement = document.querySelector('.character .lvl');
     const l_EnemyLevelElement = document.querySelector('.enemy .lvl');
@@ -172,4 +180,32 @@ function setCharacterAndEnemyLevels() {
     l_EnemyLevelElement.textContent = `Lv. ${g_Enemy.level}`;
 }
 
-gClicks = 0;
+function getRandomPokemon(pokemonArray) {
+    const l_RandomIndex = Math.floor(Math.random() * pokemonArray.length);
+    const l_RandomPokemonData = pokemonArray[l_RandomIndex];
+
+    const l_RandomPokemon = new Pokemon(
+        l_RandomPokemonData.name,
+        'enemy',
+        'progressbar-enemy',
+        'health-enemy',
+		'enemy_img',
+		l_RandomPokemonData.img,
+		l_RandomPokemonData.hp,
+    );
+	l_RandomPokemon.attacks = pokemons[l_RandomIndex].attacks;
+    return l_RandomPokemon;
+}
+
+let gClicks = 0;
+
+document.addEventListener('DOMContentLoaded', function() {
+    setCharacterAndEnemyLevels();
+	g_Character.updateMaxHealth();
+	g_Enemy.updateMaxHealth();
+});
+
+function cheat()
+{
+	g_Character.attack('other', g_Enemy, true)
+}
---
+++ js game/pokemons.js
@@ -0,0 +1,188 @@
+const pokemons = [
+    {
+        name: 'Pikachu',
+        type: 'electric',
+        hp: 274,
+        img: 'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png',
+        attacks: [
+            {
+                name: "thunder jolt",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "electro ball",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "volt tackle",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "thunder crack",
+                maxDamage: 160,
+                minDamage: 130,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        name: 'Charmander',
+        type: 'fire',
+        hp: 282,
+        attacks: [
+            {
+                name: "ember",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "flamethrower",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "burning tail",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "fire spin",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+        name: 'Bulbasaur',
+        type: 'fire',
+        hp: 294,
+        attacks: [
+            {
+                name: "tackle",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "vine whip",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "razor leaf",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "solar beam",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+        name: 'Squirtle',
+        type: 'water',
+        hp: 292,
+        attacks: [
+            {
+                name: "bubble",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "water gun",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "shell attack",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "hydro pump",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png',
+        name: 'Pidgey',
+        type: 'air',
+        hp: 284,
+        attacks: [
+            {
+                name: "Air Slash",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 100,
+            },
+            {
+                name: "Razor Wind",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 6,
+            },
+            {
+                name: "Heat Wave",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 4,
+            },
+            {
+                name: "Sky Attack",
+                maxDamage: 130,
+                minDamage: 110,
+                maxCount: 2,
+            }
+        ]
+    },
+    {
+        img: 'https://i0.wp.com/pcinvasion.com/wp-content/uploads/2016/03/gaben.jpg?ssl=1',
+        name: 'Gabe Newel',
+        type: 'air',
+        hp: 9999,
+        attacks: [
+            {
+                name: "Mega sells",
+                maxDamage: 100,
+                minDamage: 50,
+                maxCount: 100,
+            },
+            {
+                name: "Half Life 3",
+                maxDamage: -50,
+                minDamage: -70,
+                maxCount: 4,
+            },
+            {
+                name: "Chiken wings",
+                maxDamage: 500,
+                minDamage: 500,
+                maxCount: 2,
+            }
+        ]
+    }
+];
